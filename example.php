103a607a88d70b4878b63df44913ffb791a43dd4
Tosch110 <tosch110@zedat.fu-berlin.de>
2015-03-14 17:28:30 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
 require_once('easyBitcoinDark.php');
  
-$bitcoinDark = new BitcoinDark('user','pass');
+$bitcoinDark = new BitcoinDark('user','pass'); //Enter user and pass from your config file here
 
 $bitcoinDark->getinfo();
 
